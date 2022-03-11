179c96754b527b70214d730a18082730bc43a0f4
Anas-0 <anas.almutawa1@gmail.com>
2022-03-11 17:35:21 +0300

    Done

--- Homework 1/Homework 1/ContentView.swift
+++ Homework 1/Homework 1/ContentView.swift
@@ -9,8 +9,86 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("My Favorite Movies")
+                .padding(.top,25)
+            HStack {
+                Image("EndGame")
+                    .resizable()
+                    .frame(width: 110.0, height: 150)
+                    .padding([.top, .leading, .bottom])
+                Spacer()
+                Text("End Game")
+                    //.font(.title3)
+                    .font(.system(size: 25))
+                    //.padding(.trailing)
+                Spacer()
+                Text("8.5").font(.title2) //.padding(.trailing)
+                Image(systemName: "star.fill")
+                    .font(.system(size: 24))
+                    .padding(.trailing)
+                    .foregroundColor(.yellow)
+                    
+            }.background(Color.red)
+            HStack {
+                Image("TMNT")
+                    .resizable()
+                    .frame(width: 110.0, height: 150)
+                    .padding([.top, .leading, .bottom])
+                Spacer()
+                Text("TMNT")
+                    //.font(.title3)
+                    .font(.system(size: 25))
+                    //.padding(.trailing)
+                Spacer()
+                Text("8.5").font(.title2) //.padding(.trailing)
+                Image(systemName: "star.fill")
+                    .font(.system(size: 24))
+                    .padding(.trailing)
+                    .foregroundColor(.yellow)
+                    
+            }.background(Color.green)
+            
+            HStack {
+                Image("Hacksaw Ridge")
+                    .resizable()
+                    .frame(width: 110.0, height: 150)
+                    .padding([.top, .leading, .bottom])
+                Spacer()
+                Text("Hacksaw Ridge")
+                    //.font(.title3)
+                    .font(.system(size: 25))
+                    //.padding(.trailing)
+                Spacer()
+                Text("8.5").font(.title2) //.padding(.trailing)
+                Image(systemName: "star.fill")
+                    .font(.system(size: 24))
+                    .padding(.trailing)
+                    .foregroundColor(.yellow)
+                    
+            }.background(Color.blue)
+            
+            HStack {
+                Image("Spiderman nwh")
+                    .resizable()
+                    .frame(width: 110.0, height: 150)
+                    .padding([.top, .leading, .bottom])
+                Spacer()
+                Text("Spiderman nwh")
+                    //.font(.title3)
+                    .font(.system(size: 25))
+                    //.padding(.trailing)
+                Spacer()
+                Text("8.5").font(.title2) //.padding(.trailing)
+                Image(systemName: "star.fill")
+                    .font(.system(size: 24))
+                    .padding(.trailing)
+                    .foregroundColor(.yellow)
+                    
+            }.background(Color.purple)
+            
+        }
+    
     }
 }
 
